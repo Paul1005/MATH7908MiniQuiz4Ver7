eca99359d81b8a5361d7f2f44fbb98e1760645e4
Paul1005 <mccarliep@gmail.com>
2018-10-12 22:34:13 -0700

    Finished final version (probably)

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,23 +14,28 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(new FileReader("mq4data7.csv"));
-        ArrayList<ArrayList<Float>> augmentedMatrix = new ArrayList<>();
+        ArrayList<ArrayList<Double>> augmentedMatrix = new ArrayList<>();
         int matrixNum = 0;
         while (sc.hasNextLine()) {
             String[] line = sc.nextLine().split(",");
             if (line[0].startsWith("#")) {
                 if (matrixNum > 0) {
-                    System.out.println(matrixMultiplier.solveEquations(augmentedMatrix));
+                    matrixMultiplier.solveEquations(augmentedMatrix);
                 }
                 augmentedMatrix = new ArrayList<>();
                 matrixNum++;
             } else {
-                ArrayList<Float> temp = new ArrayList<>();
+                ArrayList<Double> temp = new ArrayList<>();
                 for (String number : line) {
-                    temp.add(Float.parseFloat(number));
+                    temp.add(Double.parseDouble(number));
                 }
                 augmentedMatrix.add(temp);
             }
+        }
+        ArrayList<ArrayList<Double>> matrix = matrixMultiplier.solveEquations(augmentedMatrix);
+
+        for(int i = 0; i<matrix.size(); i++){
+            System.out.println("Variable " + i + ": " + matrix.get(i).get(0));
         }
     }
 }
--- src/main/MatrixMultiplier.java
+++ src/main/MatrixMultiplier.java
@@ -5,103 +5,121 @@ import java.util.ArrayList;
 /**
  * Created by Paul on 2018-10-05.
  */
-public class MatrixMultiplier {
-    private ArrayList<ArrayList<Float>> constants = new ArrayList<>();
-    private ArrayList<ArrayList<Float>> matrix = new ArrayList<>();
+class MatrixMultiplier {
+    private ArrayList<ArrayList<Double>> constants = new ArrayList<>();
+    private ArrayList<ArrayList<Double>> matrix = new ArrayList<>();
 
-    ArrayList<ArrayList<Float>> solveEquations(ArrayList<ArrayList<Float>> augmentedMatrix) throws Exception {
-        for (int i = 0; i < augmentedMatrix.size(); i++) {
-            ArrayList<Float> temp1 = new ArrayList<>();
-            ArrayList<Float> temp2 = new ArrayList<>();
-            for (int j = 0; j < augmentedMatrix.get(i).size(); j++) {
-                if (j < augmentedMatrix.get(i).size() - 1) {
-                    temp1.add(augmentedMatrix.get(i).get(j));
+    ArrayList<ArrayList<Double>> solveEquations(ArrayList<ArrayList<Double>> augmentedMatrix) throws Exception {
+        for (ArrayList<Double> anAugmentedMatrix : augmentedMatrix) {
+            ArrayList<Double> temp1 = new ArrayList<>();
+            ArrayList<Double> temp2 = new ArrayList<>();
+            for (int j = 0; j < anAugmentedMatrix.size(); j++) {
+                if (j < anAugmentedMatrix.size() - 1) {
+                    temp1.add(anAugmentedMatrix.get(j));
                 } else {
-                    temp2.add(augmentedMatrix.get(i).get(j));
+                    temp2.add(anAugmentedMatrix.get(j));
                 }
             }
             matrix.add(temp1);
             constants.add(temp2);
         }
 
-        ArrayList<ArrayList<Float>> invertMatrix = invertMatrix(matrix);
+        ArrayList<ArrayList<Double>> invertMatrix = invertMatrix(matrix);
 
         return multiplyMatrices(invertMatrix, constants);
     }
 
-    private ArrayList<ArrayList<Float>> multiplyMatrices(ArrayList<ArrayList<Float>> matrix1, ArrayList<ArrayList<Float>> matrix2) throws Exception {
-        ArrayList<ArrayList<Float>> result = new ArrayList<>();
+    private ArrayList<ArrayList<Double>> multiplyMatrices(ArrayList<ArrayList<Double>> matrix1, ArrayList<ArrayList<Double>> matrix2) throws Exception {
+        ArrayList<ArrayList<Double>> result = new ArrayList<>();
 
-        int rows = matrix1.size();
-        int columns = matrix2.get(0).size();
+        int columnsMatrix1 = matrix1.get(0).size();
+        int rowsMatrix2 = matrix2.size();
 
-        if (rows != columns) {
+        int columnsMatrix2 = matrix2.get(0).size();
+        int rowsMatrix1 = matrix1.size();
+
+        if (columnsMatrix1 != rowsMatrix2) {
             throw new Exception();
         }
 
-        for (int i = 0; i < rows; i++) {
-            ArrayList<Float> temp1 = new ArrayList<>();
-            for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < rowsMatrix1; i++) {
+            ArrayList<Double> temp1 = new ArrayList<>();
+            for (int j = 0; j < columnsMatrix2; j++) {
+                double value = 0d;
                 for (int k = 0; k < matrix1.size(); k++) {
-                    Float value = null;
-                    for (int l = 0; l < matrix1.get(i).size(); l++) {
-                        value += matrix2.get(j).get(i) * matrix1.get(i).get(j);
-                    }
-                    temp1.add(value);
+                    value += matrix1.get(i).get(k) * matrix2.get(k).get(j);
                 }
+                temp1.add(value);
             }
             result.add(temp1);
         }
         return result;
     }
 
-    private ArrayList<ArrayList<Float>> invertMatrix(ArrayList<ArrayList<Float>> matrix) {
-        Float determinant = findDeterminant(matrix);
-        ArrayList<ArrayList<Float>> adjucate = findAdjucate(matrix);
+    private ArrayList<ArrayList<Double>> invertMatrix(ArrayList<ArrayList<Double>> matrix) {
+        Double determinant = findDeterminant(matrix);
 
-        ArrayList<ArrayList<Float>> inverse = new ArrayList<>();
+        ArrayList<ArrayList<Double>> adjucateMatrix = findAdjucate(matrix);
 
-        for (ArrayList<Float> row : adjucate) {
-            ArrayList<Float> temp = new ArrayList<>();
-            for (Float number : row) {
-                temp.add(number * determinant);
+        ArrayList<ArrayList<Double>> inverse = new ArrayList<>();
+
+        for (ArrayList<Double> row : adjucateMatrix) {
+            ArrayList<Double> temp = new ArrayList<>();
+            for (Double number : row) {
+                temp.add(number * (1 / determinant));
             }
             inverse.add(temp);
         }
         return inverse;
     }
 
-    private ArrayList<ArrayList<Float>> findAdjucate(ArrayList<ArrayList<Float>> matrix) {
-        ArrayList<ArrayList<Float>> adjucateMatrix = new ArrayList<>();
+    private ArrayList<ArrayList<Double>> findAdjucate(ArrayList<ArrayList<Double>> matrix) {
+        ArrayList<ArrayList<Double>> adjucateMatrix = new ArrayList<>();
         int sign = 1;
         for (int i = 0; i < matrix.size(); i++) {
-            ArrayList<Float> temp2 = new ArrayList<>();
-            ArrayList<ArrayList<Float>> matrix1 = new ArrayList<>();
-            for (int j = 0; j < matrix.get(j).size(); j++) {
+            ArrayList<Double> temp = new ArrayList<>();
+            for (int j = 0; j < matrix.get(i).size(); j++) {
+                ArrayList<ArrayList<Double>> determinantMatrix = new ArrayList<>();
                 for (int k = 0; k < matrix.size(); k++) {
-                    ArrayList<Float> temp1 = new ArrayList<>();
+                    ArrayList<Double> row = new ArrayList<>();
                     for (int l = 0; l < matrix.get(k).size(); l++) {
                         if (k != i && l != j) {
-                            temp1.add(matrix.get(k).get(i));
+                            row.add(matrix.get(k).get(l));
                         }
                     }
-                    matrix1.add(temp1);
+                    if (row.size() > 0) {
+                        determinantMatrix.add(row);
+                    }
                 }
-                temp2.add(sign * findDeterminant(matrix1));
+                temp.add(sign * findDeterminant(determinantMatrix));
+                sign *= -1;
             }
-            adjucateMatrix.add(temp2);
+            adjucateMatrix.add(temp);
+            if (matrix.size() % 2 == 0) {
+                sign *= -1;
+            }
         }
-        return adjucateMatrix;
+
+        ArrayList<ArrayList<Double>> transposeMatrix = new ArrayList<>();
+
+        for (int i = 0; i < adjucateMatrix.size(); i++) {
+            ArrayList<Double> row = new ArrayList<>();
+            for (int j = 0; j < adjucateMatrix.get(i).size(); j++) {
+                row.add(adjucateMatrix.get(j).get(i));
+            }
+            transposeMatrix.add(row);
+        }
+        return transposeMatrix;
     }
 
-    private Float findDeterminant(ArrayList<ArrayList<Float>> matrix) {
-        Float determinant = null;
+    private Double findDeterminant(ArrayList<ArrayList<Double>> matrix) {
+        double determinant = 0d;
         int sign = 1;
         if (matrix.size() > 2) {
             for (int i = 0; i < matrix.get(0).size(); i++) {
-                ArrayList<ArrayList<Float>> detMatrix = new ArrayList<>();
+                ArrayList<ArrayList<Double>> detMatrix = new ArrayList<>();
                 for (int j = 1; j < matrix.size(); j++) {
-                    ArrayList<Float> temp = new ArrayList<>();
+                    ArrayList<Double> temp = new ArrayList<>();
                     for (int k = 0; k < matrix.size(); k++) {
                         if (k != i) {
                             temp.add(matrix.get(j).get(k));
